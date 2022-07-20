a53d675f21748c854a08d24644300dde67f183de
Taekgil99 <pjwlover2005@naver.com>
2022-07-20 16:37:48 +0900

    [Bronze V] Title: 사분면 고르기, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,12 +1,13 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-
-        int x = sc.nextInt();
-        int y = sc.nextInt();
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
 
         if (x > 0) {
             if (y > 0) {
