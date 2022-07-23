56667650082c7caf37d0089c267b44d8e159bb4d
Taekgil99 <pjwlover2005@naver.com>
2022-07-23 17:10:10 +0900

    [Bronze V] Title: A+B - 4, Time: 84 ms, Memory: 11640 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            st = new StringTokenizer(line, " ");
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))
+                    .append('\n');
+        }
+        System.out.println(sb);
+    }
+}
