1f6d59f2c65224ffd135bc7a628e9b3b0cb40332
Taekgil99 <pjwlover2005@naver.com>
2022-08-10 21:36:29 +0900

    [Bronze III] Title: 최댓값, Time: 76 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[9];
+
+        for (int i=0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int max = 0;
+        int idx = 0;
+
+        for (int i=0; i < 9; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                idx = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(idx+1);
+    }
+}
