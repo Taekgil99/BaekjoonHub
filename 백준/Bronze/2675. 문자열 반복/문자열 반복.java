a3eec427861cc43e4889e07288211bff8b6c825c
Taekgil99 <pjwlover2005@naver.com>
2022-08-11 10:23:06 +0900

    [Bronze II] Title: 문자열 반복, Time: 72 ms, Memory: 11460 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -8,21 +8,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+
         final int T = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
 
             int R = Integer.parseInt(st.nextToken());
-            String str = st.nextToken();
 
-            for (int j = 0; j < str.length(); j++) {
-                for (int k = 0; k < R; k++) {
-                    System.out.print(str.charAt(j));
+            for (byte val : st.nextToken().getBytes()) {
+                for (int j = 0; j < R; j++) {
+                    sb.append((char) val);
                 }
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
