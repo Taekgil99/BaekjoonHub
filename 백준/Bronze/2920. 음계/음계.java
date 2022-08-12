3c39c3fd39631ddf6ce513c690343272e57ec0b9
Taekgil99 <pjwlover2005@naver.com>
2022-08-12 23:29:34 +0900

    [Bronze II] Title: 음계, Time: 76 ms, Memory: 11436 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = reader.readLine();
+
+        String ascending = "1 2 3 4 5 6 7 8";
+        String descending = "8 7 6 5 4 3 2 1";
+
+        if (line.equals(ascending)) {
+            System.out.println("ascending");
+        } else if (line.equals(descending)) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+    }
+}
