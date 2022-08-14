d71af8d89396d0fac789f8d2d50f0aaaf978ac38
Taekgil99 <pjwlover2005@naver.com>
2022-08-14 16:34:22 +0900

    [Bronze III] Title: 최소, 최대, Time: 428 ms, Memory: 121088 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -6,20 +6,26 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int max = -1000000;
         int min = 1000000;
+        
         int N = Integer.parseInt(br.readLine());
+        
         String[] lines = new String[N];
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
         for (int i = 0; i < lines.length; i++) {
             lines[i] = st.nextToken();
         }
+        
         for (String line : lines) {
-            if (max < Integer.parseInt(line)) {
-                max = Integer.parseInt(line);
+            int a = Integer.parseInt(line);
+            if (max < a) {
+                max = a;
             }
-            if (min > Integer.parseInt(line)) {
-                min = Integer.parseInt(line);
+            if (min > a) {
+                min = a;
             }
         }
         System.out.printf("%d %d", min, max);
