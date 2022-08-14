ef8e51144e069742c8635e550f135567f559be06
Taekgil99 <pjwlover2005@naver.com>
2022-08-15 08:07:17 +0900

    [Bronze IV] Title: 기찍 N, Time: 196 ms, Memory: 29544 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -6,10 +6,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
 
         for (int i = N; i > 0; i--) {
-            System.out.println(i);
-            
+            sb.append(N + "\n");
+            N--;
         }
+        System.out.println(sb);
     }
 }
