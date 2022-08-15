95ce2ea7ef4a79ebe61696ae75b12d6266afd52b
Taekgil99 <pjwlover2005@naver.com>
2022-08-15 09:46:44 +0900

    [Bronze II] Title: 숫자의 개수, Time: 72 ms, Memory: 11440 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -15,44 +15,10 @@ public class Main {
         String product = String.valueOf(A * B * C);
 
         for (int i = 0; i < product.length(); i++) {
-            char c = product.charAt(i);
-
-            switch (c) {
-                case '1':
-                    numbers[1]++;
-                    break;
-                case '2':
-                    numbers[2]++;
-                    break;
-                case '3':
-                    numbers[3]++;
-                    break;
-                case '4':
-                    numbers[4]++;
-                    break;
-                case '5':
-                    numbers[5]++;
-                    break;
-                case '6':
-                    numbers[6]++;
-                    break;
-                case '7':
-                    numbers[7]++;
-                    break;
-                case '8':
-                    numbers[8]++;
-                    break;
-                case '9':
-                    numbers[9]++;
-                    break;
-                default :
-                    numbers[0]++;
-                    break;
-            }
-
+            numbers[product.charAt(i) - 48]++;
         }
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+        for (int i : numbers) {
+            System.out.println(i);
         }
     }
 }
