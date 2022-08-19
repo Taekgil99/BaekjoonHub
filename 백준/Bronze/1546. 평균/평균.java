3d75e6fde2d1843f5aa13867773c32e76ecad3d5
Taekgil99 <pjwlover2005@naver.com>
2022-08-19 18:48:43 +0900

    [Bronze I] Title: 평균, Time: 84 ms, Memory: 11864 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,30 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        Double[] arr = new Double[Integer.parseInt(br.readLine())];
-
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Double.parseDouble(st.nextToken());
-
+        
+        int max = -1;
+        double sum = 0;
+        
+        for (int i = 0; i < N; i++) {
+            int j = Integer.parseInt(st.nextToken());
+            
+            if (j > max) {
+                max = j;
+                
+            }
+            sum += j;
+            
         }
-
-        double sum  = 0;
-
-        Arrays.sort(arr);
-
-        for (int i = 0; i < arr.length; i++) {
-            sum += ((arr[i] / arr[arr.length - 1]) * 100);
-
-        }
-        System.out.println(sum / arr.length);
+        System.out.println(((sum/max) * 100) / N);
     }
 }
