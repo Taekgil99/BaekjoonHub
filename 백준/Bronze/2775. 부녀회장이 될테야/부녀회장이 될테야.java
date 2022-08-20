3f21d6273bedbcbc717e83923d528e715fb1d325
Taekgil99 <pjwlover2005@naver.com>
2022-08-21 07:25:46 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 72 ms, Memory: 11332 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -6,38 +6,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int T = Integer.parseInt(br.readLine());
-
-        int[][] arr = new int[15][15];
+        // 아파트 [층(k), 사람(n)] 2차원배열 생성
+        // 1<= k,n <= 14
+        int[][] apt = new int[15][15];
 
         for (int i = 0; i < 15; i++) {
-            arr[i][1] = 1;
-            arr[0][i] = i;
+            //0층 i호 인원은 i
+            apt[0][i] = i;
+            //i층 1호는 1
+            apt[i][1] = 1;
         }
-
+        //1층부터
         for (int i = 1; i < 15; i++) {
+            //2호부터
             for (int j = 2; j < 15; j++) {
-                arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
+                //조건 : i층의 j인원은 i-1층의 j호까지 인원합
+                //i-1층의 j호 인원 + 1~j-1호(= i층의 이전호수 인원)
+                // -> i층 j호
+                apt[i][j] = apt[i - 1][j] + apt[i][j - 1];
             }
         }
+        //아파트 배열 완성
 
+        //테스트 케이스
+        int T = Integer.parseInt(br.readLine());
+
+        //apt[k][n] 값 구하기
         for (int i = 0; i < T; i++) {
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
-
-            sb.append(arr[k][n]).append('\n');
+            sb.append(apt[k][n]).append('\n');
         }
-
+        //결과출력
         System.out.println(sb);
-
-
-/* a,b = a-1, sum(1~b)
- * b > 1
- * k, n ??
- * a > 0, bN = N
- * 1 <= k,n <= 14
- * arr[k][n] *
- */
     }
 }
