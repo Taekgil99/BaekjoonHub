e6a43faea17f19ea2e8e47ce1bc4410936bb4ca3
taekgil99 <pjwlover2005@naver.com>
2022-09-03 11:44:40 +0900

    [Silver V] Title: 단어 정렬, Time: 824 ms, Memory: 27504 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    String[] arr = new String[N];
+    for (int i = 0; i < N; i++) {
+      arr[i] = br.readLine();
+    }
+    br.close();
+    //중복제거
+    String[] distinctArr = Arrays.stream(arr).distinct().toArray(String[]::new);
+    //정렬
+    Arrays.sort(distinctArr, new Comparator<String>() {
+      public int compare(String s1, String s2) {
+        //문자열의 길이가 같다면 사전순 정렬
+        if (s1.length() == s2.length()) {
+          return s1.compareTo(s2);
+        }
+        //다르면 길이순 정렬
+        else {
+          return s1.length() - s2.length();
+        }
+      }
+    });
+
+
+    for (String str : distinctArr) {
+      System.out.println(str);
+    }
+  }
+}
