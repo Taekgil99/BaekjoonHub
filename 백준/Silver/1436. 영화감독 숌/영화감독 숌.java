ed9341f9dc50ec479224a740263aadbb53de3f29
taekgil99 <pjwlover2005@naver.com>
2022-09-04 15:52:02 +0900

    [Silver V] Title: 영화감독 숌, Time: 356 ms, Memory: 158516 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    /**
+     * N번 째영화 = 정수중 N번 째로 666이 포함 되는 수
+     * 1부터 1씩 증가시키면서 666이 포함될 때마다 카운트 1 증가
+     * N = 카운트 일때 정수 출력
+     */
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    int answer = 1;
+
+    int count = 0;
+
+    while (count < N) {
+      answer++;
+      String str = String.valueOf(answer);
+      if (str.contains("666")) {
+        count++;
+      }
+    }
+    System.out.println(answer);
+  }
+}
