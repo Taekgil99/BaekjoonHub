e7f347f7af4e26b5fa8c0213e22e3fae3149f7e6
taekgil99 <pjwlover2005@naver.com>
2022-09-21 10:21:12 +0900

    [Silver III] Title: 01타일, Time: 104 ms, Memory: 15776 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1000001];
+
+        // N = 1
+        dp[1] = 1;
+        // N = 2
+        dp[2] = 2;
+
+        if (N > 2) {
+            for (int i = 3; i <= N; i++) {
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
