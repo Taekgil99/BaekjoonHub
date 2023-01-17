d04fcc4d3a17619828e57c99aa15b12634d7ca53
taekgil99 <pjwlover2005@naver.com>
2023-01-18 05:59:00 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    private static int answer = 0;
+	private static boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+		visited = new boolean[dungeons.length];
+		dfs(0, k, dungeons);
+		return answer;
+    }
+    
+    private void dfs(int depth, int k, int[][] dungeons) {
+        
+        for (int i = 0; i < dungeons.length; i++) {
+            if (visited[i] || k < dungeons[i][0]) {
+                continue;
+            }
+            visited[i] = true;
+            dfs(depth + 1, k - dungeons[i][1], dungeons);
+            visited[i] = false; 
+        }
+        answer = Math.max(answer, depth);
+	}
+}
